ca6e460ce1557f6356b913d285a2a96db6d38e6d
dev-tyagi25 <devanshtyagi1225@gmail.com>
2020-06-08 19:59:04 +0530

    Updates

---
+++ precache-manifest.da309466de82b02838e95b986c92d696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa72b16cb96cc2d86a5c5c6dcd0a470",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "5e6d14971be61dd90013",
+    "url": "/todoapp/static/css/main.969b942a.chunk.css"
+  },
+  {
+    "revision": "bf47b1db1fe319280f03",
+    "url": "/todoapp/static/js/2.47d05a41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.47d05a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6d14971be61dd90013",
+    "url": "/todoapp/static/js/main.c7ac7d3a.chunk.js"
+  },
+  {
+    "revision": "36c235dda1dfa6284b3d",
+    "url": "/todoapp/static/js/runtime-main.b28edc12.js"
+  }
+]);
